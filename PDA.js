509479723e46a83c792e1131452b8d88afa13552
jeevan8888 <johnwilliam199024@gmail.com>
2024-09-26 11:31:19 -0700

    update Canonical Bump

--- PDA.js
+++ PDA.js
@@ -3,6 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var web3_js_1 = require("@solana/web3.js");
 var programId = new web3_js_1.PublicKey("11111111111111111111111111111111");
 var string = "helloWorld";
-var bump = 254;
-var PDA = web3_js_1.PublicKey.createProgramAddressSync([Buffer.from(string), Buffer.from([bump])], programId);
-console.log("PDA: ".concat(PDA));
+// Loop through all bump seeds for demonstration
+for (var bump = 255; bump >= 0; bump--) {
+    try {
+        var PDA = web3_js_1.PublicKey.createProgramAddressSync([Buffer.from(string), Buffer.from([bump])], programId);
+        console.log("bump " + bump + ": " + PDA);
+    }
+    catch (error) {
+        console.log("bump " + bump + ": " + error);
+    }
+}
--- PDA.ts
+++ PDA.ts
@@ -2,11 +2,16 @@ import { PublicKey } from "@solana/web3.js";
  
 const programId = new PublicKey("11111111111111111111111111111111");
 const string = "helloWorld";
-const bump = 254;
  
-const PDA = PublicKey.createProgramAddressSync(
-  [Buffer.from(string), Buffer.from([bump])],
-  programId,
-);
- 
-console.log(`PDA: ${PDA}`);
+// Loop through all bump seeds for demonstration
+for (let bump = 255; bump >= 0; bump--) {
+  try {
+    const PDA = PublicKey.createProgramAddressSync(
+      [Buffer.from(string), Buffer.from([bump])],
+      programId,
+    );
+    console.log("bump " + bump + ": " + PDA);
+  } catch (error) {
+    console.log("bump " + bump + ": " + error);
+  }
+}
