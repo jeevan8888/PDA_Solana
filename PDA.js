f80072bd52439c18fdae63f936d7bde8de9bfca8
jeevan8888 <johnwilliam199024@gmail.com>
2024-09-26 11:28:49 -0700

    adds an optional seed 'helloworld'

--- PDA.js
+++ PDA.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var web3_js_1 = require("@solana/web3.js");
 var programId = new web3_js_1.PublicKey("11111111111111111111111111111111");
-var _a = web3_js_1.PublicKey.findProgramAddressSync([], programId), PDA = _a[0], bump = _a[1];
+var string = "helloWorld";
+var _a = web3_js_1.PublicKey.findProgramAddressSync([Buffer.from(string)], programId), PDA = _a[0], bump = _a[1];
 console.log("PDA: ".concat(PDA));
 console.log("Bump: ".concat(bump));
--- PDA.ts
+++ PDA.ts
@@ -1,8 +1,12 @@
 import { PublicKey } from "@solana/web3.js";
  
 const programId = new PublicKey("11111111111111111111111111111111");
+const string = "helloWorld";
  
-const [PDA, bump] = PublicKey.findProgramAddressSync([], programId);
+const [PDA, bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from(string)],
+  programId,
+);
  
 console.log(`PDA: ${PDA}`);
 console.log(`Bump: ${bump}`);
